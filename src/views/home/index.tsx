c4a9d6416f6a9ae153f9b29b3b58fcba3dcabb56
hugangjun <hugangjun@xiaomi.com>
2022-06-29 00:40:38 +0800

    feat: 添加@展示雏形

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useHistory } from 'react-router-dom'
 import { AtMentions } from '@/components/at-mentions'
 import './index.scss'
 
 function Home() {
+  const [pureString,setPureString]=useState('')
+  const [mentionList,setMentionList]=useState([])
   return (
     <div className='home'>
-      <div className='home__title'>@编辑器demo:</div>
-      <AtMentions className='home__at' minHeight={200} />
+      <div>
+        <div className='home__title'>@编辑器输入框：</div>
+        <AtMentions className='home__at' minHeight={200} />
+      </div>
+      <div style={{ marginLeft: '15px' }}>
+        <div className='home__title'>展示：</div>
+        <AtMentions className='home__at' minHeight={200} />
+      </div>
     </div>
   )
 }
