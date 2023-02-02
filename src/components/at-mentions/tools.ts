8cc148c6ae7a3b6c06ea9bcfd7995d1cd7f2181d
hugangjun <hugangjun@xiaomi.com>
2023-02-02 20:16:08 +0800

    fix: 调整头像,解决头像报错问题

--- src/components/at-mentions/tools.ts
+++ src/components/at-mentions/tools.ts
@@ -89,86 +89,90 @@ export const fetchUsers = async (
   searchKey: string,
   callback: { (data: any): void; (arg0: IPerson[]): void },
 ) => {
-  // 随机头像api，https://api.sunweihu.com/api/sjtx/api.php?lx=c1
+  // 随机头像api，https://api.multiavatar.com
   const testData: IPerson[] = [
     {
       userId: 'asan',
       userName: '阿三',
-      avatar: 'https://tva4.sinaimg.cn/large/9bd9b167ly1fzjxzbgbllj20b40b4mxw.jpg',
+      avatar: 'https://api.multiavatar.com',
     },
     {
       userId: 'baobo',
       userName: '鲍勃',
-      avatar: 'https://tva1.sinaimg.cn/large/9bd9b167ly1fzjxz5x8zcj20b40b4wf4.jpg',
+      avatar: 'https://api.multiavatar.com',
     },
     {
       userId: 'chensheng',
       userName: '陈胜',
-      avatar: 'https://tva1.sinaimg.cn/large/9bd9b167ly1g1p9vxh0fyj20b40b4js1.jpg',
+      avatar: 'https://api.multiavatar.com',
     },
     {
       userId: 'chengliang',
       userName: '程亮',
-      avatar: 'https://tva2.sinaimg.cn/large/9bd9b167ly1fzjxza79dzj20b40b4aas.jpg',
+      avatar: 'https://api.multiavatar.com',
     },
     {
       userId: 'chenguang',
       userName: '程光',
-      avatar: 'https://tva1.sinaimg.cn/large/9bd9b167ly1g1p9ah89uzj20b40b4aak.jpg',
+      avatar: 'https://api.multiavatar.com',
     },
     {
       userId: 'huba',
       userName: '胡巴',
-      avatar: 'https://tva2.sinaimg.cn/large/9bd9b167ly1fzjxz1srltj20b40b4js0.jpg',
+      avatar: 'https://api.multiavatar.com',
     },
     {
       userId: 'lisi',
       userName: '李四',
-      avatar: 'https://tva1.sinaimg.cn/large/9bd9b167ly1fzjxys7prej20b40b4gm5.jpg',
+      avatar: 'https://api.multiavatar.com',
     },
     {
       userId: 'likui',
       userName: '李逵',
-      avatar: 'https://tva1.sinaimg.cn/large/9bd9b167ly1fzjxyzdbd3j20b40b474v.jpg',
+      avatar: 'https://api.multiavatar.com',
     },
     {
       userId: 'lizhaodi',
       userName: '李招娣',
-      avatar: 'https://tva4.sinaimg.cn/large/9bd9b167ly1g1p9q7gsksj20b40b4aaq.jpg',
+      avatar: 'https://api.multiavatar.com',
     },
 
     {
       userId: 'liguang',
       userName: '李广',
-      avatar: 'https://tva1.sinaimg.cn/large/9bd9b167ly1g1p9ah89uzj20b40b4aak.jpg',
+      avatar: 'https://api.multiavatar.com',
     },
     {
       userId: 'liyuanfang',
       userName: '李元芳',
-      avatar: 'https://tva4.sinaimg.cn/large/9bd9b167ly1fzjxz6gmitj20b40b4js2.jpg',
+      avatar: 'https://api.multiavatar.com',
     },
     {
       userId: 'lidazhao',
       userName: '李大钊',
-      avatar: 'https://tva1.sinaimg.cn/large/9bd9b167ly1fzjxyi8a55j20b40b4mxm.jpg',
+      avatar: 'https://api.multiavatar.com',
     },
     {
       userId: 'libai',
       userName: '李白',
-      avatar: 'https://tva4.sinaimg.cn/large/9bd9b167ly1g1p9pgxgrcj20b40b4glx.jpg',
+      avatar: 'https://api.multiavatar.com',
     },
     {
       userId: 'wangwu',
       userName: '王五',
-      avatar: 'https://tva4.sinaimg.cn/large/9bd9b167ly1g1p9pdc5x9j20b40b4wep.jpg',
+      avatar: 'https://api.multiavatar.com',
     },
     {
       userId: 'zhangsan',
       userName: '张三',
-      avatar: 'https://tva4.sinaimg.cn/large/9bd9b167gy1g1p9vnmuuoj20b40b4dg2.jpg',
+      avatar: 'https://api.multiavatar.com',
     },
   ]
-  const userList: IPerson[] = testData.filter(
+  const testDataWithAvatar = testData.map((item) => {
+    item.avatar = `https://api.multiavatar.com/${item.userId}.png`
+    return item
+  })
+  const userList: IPerson[] = testDataWithAvatar.filter(
     (item) => item.userId.startsWith(searchKey) || item.userName.startsWith(searchKey),
   )
   callback(userList)
